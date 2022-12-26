38908732b4093a0171c8c02ba4f4c58a323c4482
Mansur <vakhabov.mans@gmail.com>
2022-12-26 04:02:20 +0300

    небольшие правки касательно структуры

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>hello world</div>
+  <Header/>
+  <div class="contentWrapper">Body</div>
+  <Footer/>
 </template>
 
+<!-- 
+  "@" - aliases, который ссылается на "./src/" 
+-->
+
 <script>
-  export default {}
+  import Header from "@/components/Header-comp.vue"
+  import Footer from "@/components/Footer-comp.vue"
+  export default  {
+    components: {
+      Header,
+      Footer
+    }
+  }
 </script>
 
-<style></style>
+<style></style> 
