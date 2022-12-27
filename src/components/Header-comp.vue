97f4f12ed99fad7ed39037aca5fe29dc47bedae3
Mansur <vakhabov.mans@gmail.com>
2022-12-27 23:58:31 +0300

    вынес переменные в отдельный файл

--- src/components/Header-comp.vue
+++ src/components/Header-comp.vue
@@ -1,3 +1,20 @@
 <template>
     <p>Header</p>
+    <div class={Header.headerLinksWraper} v-for="link in links" :key ="link.alt">
+        <a :href="link.url">{{ link.title }}</a>
+
+    </div>
 </template>
+
+<script>
+    import {headerLinks} from "./_config"
+    import style from "../../assets/scss/Header.css"
+    console.log(headerLinks)
+    export default {
+        data() {
+            return {
+                links: headerLinks
+            }
+        }
+    }
+</script>
---
+++ src/components/_config.js
@@ -0,0 +1,12 @@
+export const headerLinks = [
+  {
+    title: "Home",
+    url: "/",
+    alt: "Home",
+  },
+  {
+    title: "About",
+    url: "/about",
+    alt: "About",
+  },
+];
