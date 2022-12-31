f4095367b5fae0a6cfbe9ef29b3b861d73587aa0
Mansur <vakhabov.mans@gmail.com>
2023-01-01 02:56:45 +0300

    добавил ui компонент и слоты

---
+++ src/components/UI/CardComp.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <slot name="someOne"></slot>
+        {{ name }}
+        {{ role }}
+    <slot name="someTwo"></slot>
+    </div>
+    
+</template>
+
+<script>
+        export default {
+        props: {
+            name: {
+                type: String,
+            }, 
+            role: {
+                type: String,
+            }
+        }
+    };
+</script>
--- src/components/pages/homePage.vue
+++ src/components/pages/homePage.vue
@@ -1,3 +1,38 @@
 <template>
-    <div>Home page for testing</div>
+    <div v-for="item in items" :key="item.id">
+        <Card :name="item.name" :role="item.role"/> 
+        <Card>
+            <template #someTwo>{{item.name}}</template>
+            <template #someOne>{{item.name}}</template>
+        </Card>
+    </div>
+
 </template>
+
+<script>
+    import Card from '@/components/UI/CardComp.vue';
+    export default {
+        components: { Card },
+        data() {
+            return {
+                items: [
+                {
+                    id: 1,
+                    name: "Kai", 
+                    role: "sup"
+                },
+                {
+                    id: 2,
+                    name: "Kayle",
+                    role: "sin"
+                }, 
+                {
+                    id: 3,
+                    name: "Viktor",
+                    role: "cast"
+                }
+                ]
+            }
+        }
+    }
+</script>
